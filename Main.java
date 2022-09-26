e77a43b2637862a4ffb3dd33cc4827a5bf24c708
pandu63 <112938347+pandu63@users.noreply.github.com>
2022-09-26 23:42:42 +0700

    Pertemuan3
    Dalam percobaan ini saya telah membuat kode program kehadiran karyawan untuk mengetahui kehadiran dan jumlah gaji yang didapat

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        int input = Integer.parseInt(args[0]);
+
+        for (int i = 1; i < args.length; i += 3) {
+            int nik = Integer.parseInt(args[i]);
+            int gajiPokok = Integer.parseInt(args[i+2]);
+
+            karyawan k = new karyawan(nik, args[i+1], gajiPokok);
+
+            System.out.print(k.getNik() + "_" + k.getNamaLengkap());
+            k.hitungGaji();
+        }
+        String[] kehadiran = {"5-H!4-H", "5-I!4-H,", "5-H!4-S", "5-A!4-H"};
+        int pegawai5 = 0;
+        int pegawai4 = 0;
+
+        for (int i = 0; i < kehadiran.length; i++) {
+            String[] kehadiranPegawai = kehadiran[i].split("!");
+
+            for (int j = 0; j < kehadiranPegawai.length; j++) {
+                String[] dataPegawai = kehadiranPegawai[j].split("-");
+                if(Integer.parseInt(dataPegawai[0]) == 5) {
+                    if(dataPegawai[1].equals("H")){
+                        pegawai5++;
+                    }
+                } else if (Integer.parseInt(dataPegawai[0])== 4) {
+                    if (dataPegawai[1].equals("H")){
+                        pegawai4++;
+                    }
+
+                }
+            }
+        }
+        System.out.println("Kehadiran Pegawai5 : " + pegawai5);
+        System.out.println("Kehadiran Pegawai4 : " + pegawai4);
+    }
+}
---
+++ karyawan.java
@@ -0,0 +1,36 @@
+public class karyawan {
+    int nik;
+    String namaLengkap;
+    int gajiPokok;
+
+    public karyawan (int nik, String namaLengkap, int gajiPokok) {
+        this.nik = nik;
+        this.namaLengkap = namaLengkap;
+        this.gajiPokok = gajiPokok;
+    }
+
+    public int getNik() {
+        return nik;
+    }
+
+    public String getNamaLengkap(){
+        return namaLengkap;
+    }
+
+    public int gajiPokok() {
+        return gajiPokok;
+    }
+
+    public void hitungGaji() {
+        double tunjanganTransport = gajiPokok * 0.05;
+        double tunjanganKinerja = gajiPokok * 0.1;
+        double tunjMakan = 35000 * 20;
+        double totalSebelumPajak = gajiPokok + tunjanganTransport + tunjanganKinerja + tunjMakan;
+
+        double pajak = totalSebelumPajak * 0.025;
+
+        double total = totalSebelumPajak - pajak;
+
+        System.out.println(" Rp. " + total);
+    }
+}
